f8cd214eae93feaa880c84069fad5ce9e8c91e70
Dominic Owusu <dominic.owusu@ymail.com>
2022-11-22 14:03:00 +0100

    update dag and add transformation pictures

--- dags/shopdb_dag.py
+++ dags/shopdb_dag.py
@@ -39,13 +39,13 @@ print("Batch_ID = " + batch_id)
 
 task_customers_landing_to_processing = BashOperator(
  task_id="customer_landing_to_processing",
- bash_command='aws s3 mv s3://snflakedata/firehose/customers/landing2022/ s3://snflakedata/firehose/customers/processing/{0}/ --recursive'.format(batch_id),
+ bash_command='aws s3 mv s3://snflakedata/firehose/customers/landing/ s3://snflakedata/firehose/customers/processing/{0}/ --recursive'.format(batch_id),
  dag=dag
 )
  
 task_customers_processing_to_processed = BashOperator(
  task_id="customer_processing_to_processed",
- bash_command='aws s3 mv s3://snflakedata/firehose/customers/processing/{0}/ s3://snflakedata/firehose/customers/processed/{0}/ --recursive'.format(batch_id),
+ bash_command='aws s3 mv s3://snflakedata/firehose/customers/processing/ s3://snflakedata/firehose/customers/processed/{0}/ --recursive'.format(batch_id),
  dag=dag
 )
 
@@ -56,7 +56,7 @@ task_customers_processing_to_processed = BashOperator(
 task_orders_landing_to_processing = BashOperator(
     dag=dag,
     task_id = "orders_landing_to_processing" ,
-    bash_command = 'aws s3 mv s3://snflakedata/firehose/orders/landing2022/ s3://snflakedata/firehose/orders/processing/{0}/ --recursive'.format(batch_id),
+    bash_command = 'aws s3 mv s3://snflakedata/firehose/orders/landing/ s3://snflakedata/firehose/orders/processing/{0}/ --recursive'.format(batch_id),
      
 )
 
@@ -83,6 +83,18 @@ load_customers_snowflake = [
 ]
 
 
+snowflake_query_customers_orders_transformation = [
+    """ INSERT INTO ORDER_CUSTOMER_DATE_PRICE
+    (customer_name, order_date, order_total_price, batch_id)
+    (SELECT c.c_name as customer_name , o.o_orderdate as order_date, sum(o.o_totalprice) as order_total_price, c_batch_id
+from orders_raw o join customers_raw c on o.o_custkey = c.c_custkey and o_batch_id = c_batch_id
+where o_orderstatus = 'F'
+group by c_name, o_orderdate , c_batch_id
+order by o_orderdate);"""
+    
+]
+
+
 
 snowflake_orders_sql_str = SnowflakeOperator(
     task_id='insert_raw_orders_snowflake',
@@ -106,5 +118,22 @@ snowflake_customers_sql_str = SnowflakeOperator(
     role="ACCOUNTADMIN",
 )
 
+
+snowflake_customers_order_transformation= SnowflakeOperator(
+    task_id='order_customer_transformation',
+    dag=dag,
+    snowflake_conn_id=SNOWFLAKE_CONN_ID,
+    sql= snowflake_query_customers_orders_transformation,
+    warehouse="SHOP_WH",
+    database="SHOP_DB",
+    schema="SHOP_SCHEMA",
+    role="ACCOUNTADMIN",
+)
+
+
+
+
+
+
 [bash_task >>  task_customers_landing_to_processing >> snowflake_customers_sql_str >> task_customers_processing_to_processed ,
- bash_task >>task_orders_landing_to_processing >> snowflake_orders_sql_str >> task_orders_processing_to_processed]  >> post_task
+ bash_task >>task_orders_landing_to_processing >> snowflake_orders_sql_str >> task_orders_processing_to_processed] >> snowflake_customers_order_transformation >> post_task
